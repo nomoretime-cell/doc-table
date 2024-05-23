b90dfcfefeed9ef9e8c4f53c7383a5f35fd8f3cf
遥奕 <jibing.yjb@alibaba-inc.com>
2024-05-23 17:34:51 +0800

    [bugfix] [taskId]: none [taskUrl]: none [describe]: minor fix about protocol

--- main.py
+++ main.py
@@ -7,8 +7,17 @@ from pyfunvice import (
 
 
 @app_service(path="/api/v1/parser/ppl/table", inparam_type="flat")
-async def process(content_base64: str, page_idx: int, block_idx: int, table_text: str):
-    return process_table(content_base64)
+async def process(
+    content_base64: str,
+    text: str,
+    type_block_idx: int,
+    type_block_num: int,
+    block_idx: int,
+):
+    if text != "":
+        return {"text": text}
+    else:
+        return process_table(content_base64)
 
 
 if __name__ == "__main__":
