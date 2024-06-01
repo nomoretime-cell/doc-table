84306c7dece4d9c208dc97d52252ceef6a84a7b2
遥奕 <jibing.yjb@alibaba-inc.com>
2024-06-01 16:41:08 +0800

    [codereview] [taskId]: none [taskUrl]: none [describe]: add debug log

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import logging
+import os
+import threading
 from table.mathpix_cli import process_table
 from table.settings import settings
 from pyfunvice import (
@@ -5,9 +8,14 @@ from pyfunvice import (
     start_app,
 )
 
+logging.basicConfig(
+    level=logging.INFO, format="[%(asctime)s] [%(thread)d] [%(levelname)s] %(message)s"
+)
+
 
 @app_service(path="/api/v1/parser/ppl/table", inparam_type="flat")
 async def process(
+    requestId: str,
     content_base64: str,
     text: str,
     type_block_idx: int,
@@ -15,9 +23,36 @@ async def process(
     block_idx: int,
 ):
     if text != "":
+        logging.info(
+            "POST request"
+            + f" [P{os.getpid()}][T{threading.current_thread().ident}][{requestId}] "
+            + f"type_block_idx: {type_block_idx}"
+            + f", type_block_num: {type_block_num}"
+            + f", block_idx: {block_idx}"
+            + ", has_text_result: True"
+            + f", text: {text}"
+        )
         return {"text": text}
     else:
-        return process_table(content_base64)
+        logging.info(
+            "POST request"
+            + f" [P{os.getpid()}][T{threading.current_thread().ident}][{requestId}] "
+            + f"type_block_idx: {type_block_idx}"
+            + f", type_block_num: {type_block_num}"
+            + f", block_idx: {block_idx}"
+            + ", has_text_result: False"
+        )
+        result = process_table(content_base64)
+        logging.info(
+            "Return result"
+            + f" [P{os.getpid()}][T{threading.current_thread().ident}][{requestId}] "
+            + f"type_block_idx: {type_block_idx}"
+            + f", type_block_num: {type_block_num}"
+            + f", block_idx: {block_idx}"
+            + ", has_text_result: False"
+            + f", text: {result}"
+        )
+        return 
 
 
 if __name__ == "__main__":
